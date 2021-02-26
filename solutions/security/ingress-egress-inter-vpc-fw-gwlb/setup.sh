06fc582029b83bab04d1e66bad53e93e63f550ca
yossi <yossi.ros@gmail.com>
2021-02-26 02:01:06 +0000

    inter-vpc - working 0.2

--- solutions/security/ingress-egress-inter-vpc-fw-gwlb/setup.sh
+++ solutions/security/ingress-egress-inter-vpc-fw-gwlb/setup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 terraform -chdir=vpcs init
-terraform -chdir=tgw init
 terraform -chdir=vpcs plan -var-file=../admin.auto.tfvars
 read -p "Press enter to continue"
 terraform -chdir=vpcs apply -var-file=../admin.auto.tfvars --auto-approve
-terraform -chdir=tgw apply -var-file=../admin.auto.tfvars --auto-approve
 # apply
